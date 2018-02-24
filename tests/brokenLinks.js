e29d695e48b87be9d6bc862d9ad56a34ed88f49a
var-bin <var-bin@users.noreply.github.com>
2018-02-24 14:53:08 +0200

    Refactoring: Fix ESLint errors

--- tests/brokenLinks.js
+++ tests/brokenLinks.js
@@ -1,132 +1,142 @@
 // brokenLinks
 
-"use strict";
+function brokenLinks() {
+  const fs = require("fs");
+  const path = require("path");
 
-const fs = require("fs");
-const path = require("path");
-
-// https://github.com/stevenvachon/broken-link-checker
-const blc = require("broken-link-checker");
-// https://github.com/markdown-it/markdown-it
-const md = require("markdown-it")({
-  html: true,
-  linkify: true
-});
-// https://github.com/chalk/chalk
-const chalk = require("chalk");
-
-const README = "README.md";
-const ROOT = path.resolve(__dirname, "../");
-
-/**
- * Helper for checking links in HTML through `broken-link-checker`
- *
- * @param {String} html - string with HTML markup
- * @param {String} [baseUrl] - is the address to which all relative URLs will be made absolute
- */
-function brokenLinks(html, baseUrl) {
-  baseUrl = baseUrl || "";
-
-  const htmlChecker = new blc.HtmlChecker({}, {
-    link: (result) => {
-      const resultLink = result.url.original;
-      const resultBroken = result.broken ? chalk.red("  🔗 Broken") : chalk.green("  🔗 OK");
-
-      if (resultBroken) {
-        const msg = `${resultBroken} ${resultLink}`;
-
-        console.info(msg);
-      }
-
-      if (result.broken) {
-        htmlChecker.pause();
-        const errMsg = `Link ${result.url.original} is broken ${result.brokenReason}. Please check it and fix it!`;
-
-        throw new Error(errMsg);
-      }
-    },
-    complete: () => {
-      const endMsg = chalk.bgYellow("\n=== Finish broken link checker ===");
-
-      console.info(endMsg);
-    }
+  // https://github.com/stevenvachon/broken-link-checker
+  const blc = require("broken-link-checker");
+  // https://github.com/markdown-it/markdown-it
+  const md = require("markdown-it")({
+    html: true,
+    linkify: true
   });
+  // https://github.com/chalk/chalk
+  const chalk = require("chalk");
 
-  const initMsg = chalk.bgYellow("=== Init broken link checker ===\n");
-  console.info(initMsg);
+  const README = "README.md";
+  const ROOT = path.resolve(__dirname, "../");
 
-  htmlChecker.scan(html, baseUrl);
-}
+  /**
+   * Helper for checking links in HTML through `broken-link-checker`
+   *
+   * @param {String} html - string with HTML markup
+   * @param {String} [baseUrl] - is the address to which all relative URLs will be made absolute
+   */
+  function _brokenLinks(html, baseUrl) {
+    const _baseUrl = baseUrl || "";
 
-/**
- * Do links checking for markdown files
- *
- * @param {String} err - Error thrown by `fs.readFile`
- * @param {String} data - File content is given by `fs.readFile`
- */
-function checkLinks(err, data) {
-  if (err) {
-    throw err;
+    const htmlChecker = new blc.HtmlChecker({}, {
+      link: (result) => {
+        const resultLink = result.url.original;
+        const resultBroken = result.broken ? chalk.red("  🔗 Broken") : chalk.green("  🔗 OK");
+
+        if (resultBroken) {
+          const msg = `${resultBroken} ${resultLink}`;
+
+          console.info(msg);
+        }
+
+        if (result.broken) {
+          htmlChecker.pause();
+          const errMsg = `Link ${result.url.original} is broken ${result.brokenReason}. Please check it and fix it!`;
+
+          throw new Error(errMsg);
+        }
+      },
+      complete: () => {
+        const endMsg = chalk.bgYellow("\n=== Finish broken link checker ===");
+
+        console.info(endMsg);
+      }
+    });
+
+    const initMsg = chalk.bgYellow("=== Init broken link checker ===\n");
+    console.info(initMsg);
+
+    htmlChecker.scan(html, _baseUrl);
   }
 
-  brokenLinks(md.render(data));
-}
-
-/**
- * Recursively iterate through project directories tree. Found all README.md, convert it to HTML and do checks
- *
- * @param {String} dir - Name of directory for starting do recursively checking
- * @param {Function} callback - Function with two args.
- */
-function getFiles(dir, callback) {
-  dir = dir || "";
-  callback = callback || undefined;
-
-  const TRIGGERS_FILES_DIRECTORIES = {
-    ".git": ".git",
-    ".gitignore": ".gitignore",
-    "node_modules": "node_modules",
-    "LICENSE": "LICENSE",
-    "tests": "tests"
-  };
-
-  fs.readdir(dir, (err, items) => {
+  /**
+   * Do links checking for markdown files
+   *
+   * @param {String} err - Error thrown by `fs.readFile`
+   * @param {String} data - File content is given by `fs.readFile`
+   */
+  function checkLinks(err, data) {
     if (err) {
       throw err;
     }
 
-    const newItems = items.filter((item) => {
-      return item !== TRIGGERS_FILES_DIRECTORIES[item];
-    });
+    _brokenLinks(md.render(data));
+  }
 
-    newItems.forEach((newItem) => {
-      fs.stat(newItem, (err, stats) => {
-        if (err) {
-          throw err;
-        }
+  /**
+   * Recursively iterate through project directories tree.
+   * Found all README.md, convert it to HTML and do checks
+   *
+   * @param {String} dir - Name of directory for starting do recursively checking
+   * @param {Function} callback - Function with two args:
+   * err - Error thrown by `fs.readFile`
+   * data - File content is given by `fs.readFile`
+   */
+  function getFiles(dir, callback) {
+    const _dir = dir || "";
+    const _callback = callback || undefined;
 
-        if (stats.isFile() && path.basename(newItem) === README) {
-          console.log(chalk.underline(`${path.resolve(dir, newItem)}: isFile`));
+    const TRIGGERS_FILES_DIRECTORIES = {
+      ".git": ".git",
+      ".gitignore": ".gitignore",
+      node_modules: "node_modules",
+      LICENSE: "LICENSE",
+      tests: "tests"
+    };
 
-          if (typeof callback === "function") {
-            fs.readFile(path.resolve(dir, newItem), "utf8", callback);
+    fs.readdir(_dir, (err, items) => {
+      if (err) {
+        throw err;
+      }
+
+      const newItems = items.filter(item => item !== TRIGGERS_FILES_DIRECTORIES[item]);
+
+      newItems.forEach((newItem) => {
+        fs.stat(newItem, (statErr, stats) => {
+          if (statErr) {
+            throw statErr;
           }
-        }
 
-        // if `newItem` is directory, read it
-        if (stats.isDirectory()) {
-          console.log(chalk.italic(`${path.resolve(dir, newItem)}: isDirectory`));
+          if (stats.isFile() && path.basename(newItem) === README) {
+            console.info(chalk.underline(`${path.resolve(_dir, newItem)}: isFile`));
 
-          // magic happens here
-          getFiles(path.relative(dir, newItem), (err, data) => {
-            checkLinks(err, data);
-          });
-        }
+            if (typeof _callback === "function") {
+              fs.readFile(path.resolve(_dir, newItem), "utf8", _callback);
+            }
+          }
+
+          // if `newItem` is directory, read it
+          if (stats.isDirectory()) {
+            console.info(chalk.italic(`${path.resolve(_dir, newItem)}: isDirectory`));
+
+            // magic happens here
+            getFiles(path.relative(_dir, newItem), (readFileErr, readFileErrData) => {
+              checkLinks(readFileErr, readFileErrData);
+            });
+          }
+        });
       });
     });
-  });
+  }
+
+  function _init() {
+    getFiles(ROOT, (readFileErr, readFileErrData) => {
+      checkLinks(readFileErr, readFileErrData);
+    });
+  }
+
+  return {
+    init: _init
+  };
 }
 
-getFiles(ROOT, (err, data) => {
-  checkLinks(err, data);
-});
+const bl = brokenLinks();
+bl.init();
